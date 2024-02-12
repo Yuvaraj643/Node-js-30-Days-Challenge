1f0da5ff96220b0fe0496f894565a50e8664a66e
Yuvaraj643 <vyuvarajnani2001@gmail.com>
2024-02-12 15:02:38 +0530

    Day-12

---
+++ Express Rate Limiting-Day-12/Day12.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const rateLimit = require('express-rate-limit');
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 5,
+    message: 'Too many requests from this IP, please try again later.',
+});
+
+app.use(limiter);
+
+app.get('/', (req, res) => {
+    res.send('Day 12 Completed');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
